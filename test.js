000fa9fa5e334097228528ed4fd2126dfd014b77
Nuno Teixeira <nuno@codacy.com>
2016-08-03 11:51:07 +0100

    add duplicated function

--- test.js
+++ test.js
@@ -1,3 +1,24 @@
+function secondMain(meaning) {
+  var day = true;
+
+  if (meaning == 42) {
+     if(1 === 2) return
+     
+    while (day) {
+      shuffle();
+
+      if (tired) { // JSHint: Blocks are nested too deeply (3).
+          sleep();
+      }
+      else {
+         if(true) return false;
+         console.log(123);
+      }
+      eval(23)
+    }
+  }
+}
+
 function updateManifestVersion(filename, version) {
    readFile("")
    if(a === b) {
@@ -29,5 +50,3 @@ function main(meaning) {
     }
   }
 }
-
-
