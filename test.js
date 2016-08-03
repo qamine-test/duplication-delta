000fe1b225860926d234a63bef3df4f208ace7ce
Nuno Teixeira <nuno@codacy.com>
2016-08-03 11:56:57 +0100

    add cross file duplication

--- test.js
+++ test.js
@@ -50,3 +50,23 @@ function main(meaning) {
     }
   }
 }
+
+function initialize() {
+        for (var i = 0; i < values.count; i++) {
+                var offset = new paper.Point(20 + 10 * i, 0);
+                var path = new paper.Path();
+                path.fillColor = i % 2 ? 'red' : 'black';
+                path.closed = true;
+
+                var l = offset.length;
+                for (var j = 0; j < values.points * 2; j++) {
+                        offset.angle += 360 / values.points;
+                        var vector = offset.normalize(l * (j % 2 ? 0.1 : -0.1));
+                        path.add(offset.add(vector));
+                }
+                path.smooth();
+                layer.insertChild(0, path);
+        }
+        layer.fitBounds(paper.view.bounds);
+}
+
