00083bae4e63d59fe8eb5fa5eece32d673c44ba7
Nuno Teixeira <nuno@codacy.com>
2016-08-03 11:50:29 +0100

    initial commit

---
+++ test.js
@@ -0,0 +1,33 @@
+function updateManifestVersion(filename, version) {
+   readFile("")
+   if(a === b) {
+      return
+   }
+}
+
+function readFile(filename) {
+   eval(1);
+}
+
+function main(meaning) {
+  var day = true;
+
+  if (meaning == 42) {
+     if(1 === 2) return
+     
+    while (day) {
+      shuffle();
+
+      if (tired) { // JSHint: Blocks are nested too deeply (3).
+          sleep();
+      }
+      else {
+         if(true) return false;
+         console.log(123);
+      }
+      eval(23)
+    }
+  }
+}
+
+
